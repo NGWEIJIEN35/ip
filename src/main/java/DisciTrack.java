ecf3e4d60a95fce3305dcc332d496042ea6bbd19
NGWEIJIEN35 <ngweijien35@gmail.com>
2026-08-20 21:03:55 +0800

    Add mark as done feature

--- src/main/java/DisciTrack.java
+++ src/main/java/DisciTrack.java
@@ -21,25 +21,43 @@ public class DisciTrack {
 
         Scanner scanner = new Scanner(System.in); //to receive users input
 
-        List<String> list_of_tasks = new ArrayList<>();
+        List<Task> listOfTasks = new ArrayList<>();
 
-        while(true) {
+        while (true) {
             String command = scanner.nextLine();
-            if(command.equals("bye")) {
+            if (command.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye bye! Well done today, keep it up! Hope to see you again soon!");
                 System.out.println(line);
                 break;
-            } else if(command.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println(line);
 
-                for(int i = 0; i < list_of_tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + list_of_tasks.get(i));
+                for (int i = 0; i < listOfTasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + listOfTasks.get(i));
                 }
 
                 System.out.println(line);
-            }else {
-                list_of_tasks.add(command);
+            } else if (command.startsWith("mark ")) {  //assume users follow command's format, could modify in future
+                String taskNumberString = command.substring(5);  //get the task user chose
+                int taskNumber = Integer.parseInt(taskNumberString);
+                Task task = listOfTasks.get(taskNumber - 1);
+                task.markAsDone();
+                System.out.println(line);
+                System.out.println("Well done, I have marked this task as done!");
+                System.out.println(task);
+                System.out.println(line);
+            } else if (command.startsWith("unmark ")) {
+                String taskNumberString = command.substring(7);
+                int taskNumber = Integer.parseInt(taskNumberString);
+                Task task = listOfTasks.get(taskNumber - 1);
+                task.markAsUndone();
+                System.out.println(line);
+                System.out.println("I have marked this task as not done yet, try to finish soon!");
+                System.out.println(task);
+                System.out.println(line);
+            } else {
+                listOfTasks.add(new Task(command));
                 System.out.println(line);
                 System.out.println("I have added: " + command);
                 System.out.println(line);
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    private String activity; //tasks that users need to finish
+    private boolean status;  //whether it is done or not
+
+    public Task(String activity) {
+        this.activity = activity;
+        this.status = false;
+    }
+
+    public void markAsDone() {
+        this.status = true;
+    }
+
+    public void markAsUndone() {
+        this.status = false;
+    }
+
+    public String checkStatus() {   //to get symbols for checklist boxes
+        return this.status ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + checkStatus() + "] " + this.activity;
+    }
+}
