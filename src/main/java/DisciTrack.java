6701efeb139964f4a8b09d8f25383e187356a074
NGWEIJIEN35 <ngweijien35@gmail.com>
2026-08-20 18:37:39 +0800

    Add and store users input in list

--- src/main/java/DisciTrack.java
+++ src/main/java/DisciTrack.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DisciTrack {
     public static void main(String[] args) {
@@ -13,23 +15,34 @@ public class DisciTrack {
                 + "Hello! I am DisciTrack.\n"
                 + "My job is to keep your discipline on track.\n"
                 + "How can I help you?\n"
-                + line + "\n";
+                + line;
 
         System.out.println(greeting);
 
         Scanner scanner = new Scanner(System.in); //to receive users input
 
+        List<String> list_of_tasks = new ArrayList<>();
+
         while(true) {
             String command = scanner.nextLine();
-            if(!command.equals("bye")) {
-                System.out.println(line);
-                System.out.println(command);
-                System.out.println(line);
-            } else {
+            if(command.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye bye! Well done today, keep it up! Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if(command.equals("list")) {
+                System.out.println(line);
+
+                for(int i = 0; i < list_of_tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + list_of_tasks.get(i));
+                }
+
+                System.out.println(line);
+            }else {
+                list_of_tasks.add(command);
+                System.out.println(line);
+                System.out.println("I have added: " + command);
+                System.out.println(line);
             }
         }
     }
