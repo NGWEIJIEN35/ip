f73a883541a69f6cd09dd5262251d2d8a1d2a395
NGWEIJIEN35 <ngweijien35@gmail.com>
2026-08-20 17:14:56 +0800

    Update the initial code to greet the user and exit

--- src/main/java/DisciTrack.java
+++ src/main/java/DisciTrack.java
@@ -1,10 +1,20 @@
 public class DisciTrack {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String line = "____________________________________________________________";
+        String banner = line + "\n"
+                + " ____    _             _   _____                 _       \n"
+                + "|  _ \\  (_) ___   ___ (_) |_   _| _ __   __ _  ___ | | __ \n"
+                + "| | | | | |/ __| / __|| |   | |  | '__| / _` |/ __|| |/ / \n"
+                + "| |_| | | |\\__ \\| (__ | |   | |  | |   | (_| | (__ |   <  \n"
+                + "|____/  |_||___/ \\___||_|   |_|  |_|    \\__,_|\\___||_|\\_\\ \n"
+                + line + "\n"
+                + "Hello! I am DisciTrack.\n"
+                + "My job is to keep your discipline on track.\n"
+                + "How can I help you?\n"
+                + line + "\n"
+                + "Bye. Hope to see you again!\n"
+                + line;
+
         System.out.println(banner);
     }
 }
